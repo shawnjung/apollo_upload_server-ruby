4520f6aca5235f53b844723b0e68817adff4ab20
Shawn Jung <shawn.jung@me.com>
2020-07-08 13:17:51 -0700

    parsing graphql related operation should only happen when the request is from graphql (maybe bug on Rails 3.2

--- lib/apollo_upload_server/middleware.rb
+++ lib/apollo_upload_server/middleware.rb
@@ -7,10 +7,10 @@ module ApolloUploadServer
     end
 
     def call(env)
-      request = ActionDispatch::Request.new(env)
-      params = request.params
+      params = env['action_dispatch.request.request_parameters']
 
       if env['CONTENT_TYPE'].to_s.include?('multipart/form-data') && params['operations'].present? && params['map'].present?
+        request = ActionDispatch::Request.new(env)
         result = GraphQLDataBuilder.new.call(request.params)
         result.each do |key, value|
           request.request_parameters[key] = value
